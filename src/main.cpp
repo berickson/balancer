143b211a7b12e6538a0d7ab800a7f12e2abb32a1
Brian Erickson <berickson@gmail.com>
2019-07-10 15:41:18 -0700

    Wifi working

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 #include "OLEDDisplay.h"
 #include "math.h"
 #include "BluetoothSerial.h"
+#include "WiFi.h"
+#include "secret.h"
 
 
 
@@ -17,6 +19,8 @@ const int oled_address=0x3c;
 const int pin_oled_sda = 4;
 const int pin_oled_sdl = 15;
 const int pin_oled_rst = 16;
+
+const int pin_led = 25;
 
 const int pin_touch = T4;
 
@@ -110,6 +114,62 @@ Button button;
 const uint32_t bluetooth_buffer_reserve = 500;
 BluetoothSerial bluetooth;
 MPU6050 mpu;
+WiFiServer server(80);
+
+void wifi_loop(){
+ WiFiClient client = server.available();   // listen for incoming clients
+ pinMode(25, OUTPUT);
+
+  if (client) {                             // if you get a client,
+    //Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        //Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("The LED is ");
+            client.print(digitalRead(pin_led)?"ON":"OFF");
+            client.print("<br>");
+            client.print( "Click <a href='/H'>here</a> to turn the LED on.<br>" );
+            client.print( "Click <a href='/L'>here</a> to turn the LED off.<br>" );
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(25, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(25, LOW);                // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    //Serial.println("Client Disconnected.");
+  }
+}
 
 void setup() {
 
@@ -117,6 +177,7 @@ void setup() {
   Serial.begin(115200);
   button.init(pin_touch);
   bluetooth.begin("bke");
+  
   pinMode(pin_oled_rst, OUTPUT);
   digitalWrite(pin_oled_rst, LOW);
   delay(10);
@@ -126,17 +187,33 @@ void setup() {
   // init display before mpu since it initializes shared i2c
   display.init();
 
-  Serial.println("Initializing I2C devices...");
+  Serial.println("Initializing mpu...");
   mpu.initialize();
 
   // verify connection
-  Serial.println("Testing device connections...");
+  Serial.println("Testing mpu connection...");
   Serial.println(mpu.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
   mpu.reset();
   delayMicroseconds(50000);
   mpu.setSleepEnabled(false);
   mpu.setTempSensorEnabled(true);
   mpu.setFullScaleAccelRange(0);
+
+
+  // Wifi
+  // see https://github.com/espressif/arduino-esp32/blob/master/libraries/WiFi/examples/SimpleWiFiServer/SimpleWiFiServer.ino
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  server.begin();
+  Serial.println("");
+  Serial.println("WiFi connected to ");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
     
 
   display.println("Setup complete");
@@ -173,7 +250,9 @@ void loop() {
   static unsigned long last_loop_ms = 0;
   unsigned long loop_ms = millis();
 
+
   if(every_n_ms(loop_ms, last_loop_ms, 1)) {
+    wifi_loop();
     // read the button
     button.execute();
 
@@ -203,6 +282,8 @@ void loop() {
     display.drawString(0, 0, String("t:")+String(t));
     display.drawString(0, 10, String("accel[") +String(ax)+","+String(ay)+","+String(az)+String("]"));
     display.drawString(0, 20, String("gyro[") +String(gx)+","+String(gy)+","+String(gz)+String("]"));
+    display.drawString(0, 30, WiFi.localIP().toString());
+    display.drawString(0, 40, last_bluetooth_line);
     display.display();
   }
   ++loop_count;
