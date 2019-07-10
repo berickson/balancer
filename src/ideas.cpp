8714cc41d36087b6e21ea67cc15cb1a1dad39d37
Brian Ericksons <berickson@gmail.com>
2019-07-10 14:48:04 -0700

    Create ideas.cpp

---
+++ src/ideas.cpp
@@ -0,0 +1,10 @@
+// wifi task keeps wifi asynchronous
+// auto connects
+// wifi can be setup from bluetooth
+// returns lines, or whole documents?
+// shared routing of commands with bluetooth and serial via put command
+// get command returns web page with set of all put commands
+class WifiTask {
+  void execute();
+  get_line();
+}
